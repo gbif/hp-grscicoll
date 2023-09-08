0521e89a6c51bdc68c98e007c9378eb16045a8fd
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-09-08 10:33:40 +0200

    change config to v2

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,11 +1,5 @@
 var siteTheme = gbifReactComponents.themeBuilder.extend({
   baseTheme: 'light', extendWith: {
-    primary: themeStyle.colors.primary
-  }
-});
-
-var siteTheme = gbifReactComponents.themeBuilder.extend({
-  baseTheme: 'light', extendWith: {
     primary: themeStyle.colors.primary,
     fontSize: '16px'
   }
@@ -13,30 +7,12 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({
 
 
 var siteConfig = {
+  version: 2,
   routes: {
+    enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey'],
     occurrenceSearch: {
-      // The route you are currently using for occurrence search. The language prefix will be added automatically
-      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
       route: '/specimen/search'
-    },
-    collectionKey: {
-      route: '/collection/:key',
-      isHref: true,
-      url: ({ key }) => {
-        return `/collection/${key}`;
-      }
-    },
-    collectionSpecimens: {
-      route: '/collection/:key/specimens',
-      url: ({ key }) => `/collection/${key}/specimens`
-    },
-    institutionKey: {
-      route: '/institution/:key',
-      isHref: true,
-      url: ({ key }) => {
-        return `/institution/${key}`;
-      }
-    },
+    }
   },
   occurrence: {
     excludedFilters: ['occurrenceStatus', 'networkKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
