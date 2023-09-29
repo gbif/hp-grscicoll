cc43d6f2f61f9606f1f5c71e2db21ec958aa1d08
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-09-29 14:38:33 +0200

    fix: config to support translations

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -10,7 +10,7 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({
 var siteConfig = {
   version: 2,
   routes: {
-    // alwaysUseHrefs: true, // since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
+    alwaysUseHrefs: true, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
     enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey'],
     occurrenceSearch: {
       route: '/specimen/search'
